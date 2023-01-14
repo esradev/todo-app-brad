5c187f99f414b88478b28bef2906974ae1657c54
Wpstorm <wpstormdev@gmail.com>
2023-01-15 00:50:38 +0330

    Now the app adds item into the MongoDB TodoApp Db

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 let express = require("express");
+let mongodb = require("mongodb").MongoClient;
 
 let app = express();
+let db;
+
+let connectionString =
+  "mongodb+srv://wpstorm:WKci4uEuz05dDFDF@cluster0.w4ivryt.mongodb.net/TodoApp?retryWrites=true&w=majority";
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    db = client.db();
+    app.listen(3000);
+  }
+);
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -59,8 +72,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/create-item", function (req, res) {
-  console.log(req.body.item);
-  res.send("Thanks for submitting the form.");
+  db.collection("items").insertOne({ text: req.body.item }, function () {
+    res.send("Thanks for submitting the form.");
+  });
 });
-
-app.listen(3000);
