159fa426abddda75777aa836a2b8df5e2c956e96
Wpstorm <wpstormdev@gmail.com>
2023-01-15 10:50:59 +0330

    Now the user can create items on the fly without reloading page.

--- public/browser.js
+++ public/browser.js
@@ -1,16 +1,45 @@
+// Create Feature
+let createField = document.getElementById("create-field");
+
+function itemTemplate(item) {
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  <span class="item-text">${createField.value}</span>
+  <div>
+  <button data-id="${item.insertedId}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+  <button data-id="${item.insertedId}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  </div>
+  </li>`;
+}
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  axios
+    .post("/create-item", { text: createField.value })
+    .then(function (response) {
+      console.log(response);
+      // Create the HTML for a new item
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+      createField.value = "";
+      createField.focus();
+    })
+    .catch(function () {
+      console.log("Please try again later.");
+    });
+});
+
 document.addEventListener("click", function (e) {
   // Delete Feature
   if (e.target.classList.contains("delete-me")) {
-    if (confirm("Do you really want delete this item permanently?")) {
+    if (confirm("Do you really want to delete this item permanently?")) {
       axios
-        .post("/delete-item", {
-          id: e.target.getAttribute("data-id"),
-        })
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
         .then(function () {
           e.target.parentElement.parentElement.remove();
         })
         .catch(function () {
-          console.log("please try again later.");
+          console.log("Please try again later.");
         });
     }
   }
@@ -33,7 +62,7 @@ document.addEventListener("click", function (e) {
           ).innerHTML = userInput;
         })
         .catch(function () {
-          console.log("please try again later.");
+          console.log("Please try again later.");
         });
     }
   }
